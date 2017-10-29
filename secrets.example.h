c8deb3c16ded6176fcd7aa490b8d8634a234d507
slurdge <slurdge@slurdge.org>
2017-10-29 23:03:08 +0100

    Add examples of secrets files

---
+++ secrets.example.h
@@ -0,0 +1,11 @@
+#ifndef SECRETS_INCLUDED
+#define SECRETS_INCLUDED
+
+#define WIFI_NAME "wifi"
+#define WIFI_PASS "pass"
+#define API_HOSTNAME "example.com"
+#define API_ENDPOINT "/data"
+#define API_PORT 443
+#define API_AUTH_TOKEN "70ch4n63454p"
+
+#endif // !SECRETS_INCLUDED
---
+++ web/data/secrets.example.php
@@ -0,0 +1,3 @@
+<?php
+$sha1_of_auth = 'fa87a93bb99e87529d2ff4c563af1fc2e27aa747';
+?>
